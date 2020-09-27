96d88c8014d1dc09092826f051caea04b2999490
Ming-Tso Wei <mingtsoweii@gmail.com>
2020-09-26 23:14:23 -0400

    Change it to a function with two inputs: nskiprows and usecols_list

--- load_csv_umd_to_mat.py
+++ load_csv_umd_to_mat.py
@@ -1,46 +1,47 @@
-from glob import glob
-import os
-import re
-import matplotlib
-matplotlib.use("TkAgg")
-import numpy as np
-import matplotlib.pyplot as plt
-import tkinter as tk
-from scipy.io import savemat
+def load_csv_to_mat(nskiprows,usecols_list):
+    from glob import glob
+    import os
+    import re
+    import matplotlib
+    matplotlib.use("TkAgg")
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import tkinter as tk
+    from scipy.io import savemat
 #from re import match
-from tkinter import filedialog
+    from tkinter import filedialog
 
 # open a dialog asking for first file to load
-root=tk.Tk()
-root.withdraw()
-root.filename=filedialog.askopenfilename(initialdir ='~/')
+    root=tk.Tk()
+    root.withdraw()
+    root.filename=filedialog.askopenfilename(initialdir ='~/')
 #Move and print the directory
-directory=os.path.dirname(root.filename)
-os.chdir(directory)
-directory=directory+'/' # Change the orientation of the slash for Windows or Mac systems
-print(directory)
-first, ext=os.path.splitext(root.filename)
+    directory=os.path.dirname(root.filename)
+    os.chdir(directory)
+    directory=directory+'/' # Change the orientation of the slash for Windows or Mac systems
+    print(directory)
+    first, ext=os.path.splitext(root.filename)
 #a list of files under this directory
-all_f=glob(directory+r'*'+ext)
-all_files=sorted(all_f,key=os.path.getmtime)
+    all_f=glob(directory+r'*'+ext)
+    all_files=sorted(all_f,key=os.path.getmtime)
 #all_files=sorted(all_f,key=os.path.basename)
-prefix = os.path.basename(os.path.commonprefix(all_files))
+    prefix = os.path.basename(os.path.commonprefix(all_files))
 
-print("There are "+ str(len(all_files))+" "+ext+" files in this directory starting with "+prefix)
+    print("There are "+ str(len(all_files))+" "+ext+" files in this directory starting with "+prefix)
 
 #load the first data and get the dimension of the matrix
 #matrix (# of rows, # of columns, # of files)
 
-data1=np.loadtxt(root.filename,delimiter=',',skiprows=54,usecols=(2,3,4,5))# check the columns
-saiz=[data1.shape[0],data1.shape[1],len(all_files)]
-matrix=np.empty(saiz)
-print(saiz)
-index=0
-for files in all_files:
-    temp=np.loadtxt(files,delimiter=',',skiprows=54,usecols=(2,3,4,5))# check the columns
-    matrix[:,:,index]=temp
-    print(str(files))
-    index=index+1
+    data1=np.loadtxt(root.filename,delimiter=',',skiprows=nskiprows,usecols=usecols_list)# check the columns
+    saiz=[data1.shape[0],data1.shape[1],len(all_files)]
+    matrix=np.empty(saiz)
+    print(saiz)
+    index=0
+    for files in all_files:
+        temp=np.loadtxt(files,delimiter=',',skiprows=nskiprows,usecols=usecols_list)# check the columns
+        matrix[:,:,index]=temp
+        print(str(files))
+        index=index+1
 #save the .mat files for MATLAB
-savemat(prefix+'.mat',mdict={'matrix':matrix})
-print("The size of the saved matrix is "+ str(matrix.shape))
+    savemat(prefix+'.mat',mdict={'matrix':matrix})
+    print("The size of the saved matrix is "+ str(matrix.shape))
