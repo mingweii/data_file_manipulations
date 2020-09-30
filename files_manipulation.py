889634d50c1c1eeba03175ea60d942e73d397343
mingweii <mingtsoweii@gmail.com>
2020-09-30 12:49:42 -0400

    Add notebook for merging mat files
    fix some buys of the merging function.

--- files_manipulation.py
+++ files_manipulation.py
@@ -45,32 +45,33 @@ def merge_multiple_mat(dataDir,save_to_h5=False):
 
     sorted_file_list=sorted(os.listdir(dataDir)) # sort the file list first by name
     for file in sorted_file_list:
-        d=loadmat(dataDir+file)
-        if merged_data=={}:
-            merged_data=d.copy()
-            merged_data.pop('__globals__')
-            merged_data.pop('__header__')
-            merged_data.pop('__version__')
-            for i in merged_data.keys():
-                merged_data[i]=np.round(np.squeeze(merged_data[i]),10)
-        else:
-            for i in d.keys() and merged_data.keys():
-                d[i]=np.round(np.squeeze(d[i]),10)
-                if np.array_equal(merged_data[i],d[i]) and (d[i]).ndim==1 and len(d[i])>1 :
-                    # Only check 1D array with multiple elements.
-                    merged_data[i]=merged_data[i]
-                else:
-                    merged_data[i]=np.dstack((merged_data[i],d[i]))
+        if not file=='merged.mat' and not file=='merged.h5' :
+            d=loadmat(dataDir+file)
+            if merged_data=={}:
+                merged_data=d.copy()
+                merged_data.pop('__globals__')
+                merged_data.pop('__header__')
+                merged_data.pop('__version__')
+                for i in merged_data.keys():
+                    merged_data[i]=np.round(np.squeeze(merged_data[i]),10)
+            else:
+                for i in d.keys() and merged_data.keys():
+                    d[i]=np.round(np.squeeze(d[i]),10)
+                    if np.array_equal(merged_data[i],d[i]) and (d[i]).ndim==1 and len(d[i])>1 :
+                            # Only check 1D array with multiple elements.
+                        merged_data[i]=merged_data[i]
+                    else:
+                        merged_data[i]=np.dstack((merged_data[i],d[i]))
     for i in merged_data.keys():
 
         merged_data[i]=np.squeeze(merged_data[i])
         print(str(i)+".shape="+str(merged_data[i].shape))
     if save_to_h5==False:
-        savemat('merged.mat',merged_data)
+        savemat(dataDir+'merged.mat',merged_data)
     else:
-        if os.path.exists("merged.h5"):
-            os.remove("merged.h5")
-        with h5py.File('merged.h5', 'w') as fd:
+        if "merged.h5" in os.listdir(dataDir):
+            os.remove(dataDir+"merged.h5")
+        with h5py.File(dataDir+"merged.h5", 'w') as fd:
             for i in merged_data.keys():
                 fd[i] = merged_data[i]
             fd.close()
